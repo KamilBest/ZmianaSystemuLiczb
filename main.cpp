29dcc48453c3024f5e094ae71042ae091a9445a7
Kamil Best <kamil.besst@gmai.com>
2014-10-04 21:06:22 +0200

    remake to class

--- main.cpp
+++ main.cpp
@@ -7,41 +7,70 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	string liczba;
-	char repeat = 't'; //do pętli powtarzającej program
+class Na_dziesietny {
+	string s_liczba;
+	unsigned i_podstawa;
+	int dlugosc;
+	unsigned i_wynik;
+public:
+	void powitanie() {
 
-	unsigned podstawa, wynik, cyfra;
+		cout << "Obliczanie wartości liczby zapisanej"
+				<< "\nw podanym systemie pozycyjnym"
+				<< "\nprzy pomocy schematu Hornera" << endl;
 
-	cout << "Obliczanie wartości liczby zapisanej"
-			<< "\nw podanym systemie pozycyjnym"
-			<< "\nprzy pomocy schematu Hornera" << endl;
-
-	while (repeat == 't') { //pętla powtarzająca program dopóki użytkownik wpisze t
-
+	}
+	void pobierz_dane() {
 		cout << "\nPodaj liczbe    = ";
-		cin >> liczba;
-		int dlugosc = liczba.length();
+		cin >> s_liczba;
 
 		do { //sprawdzanie poprawności podstawy
 			cout << endl << "Podaj podstawę (2..9) = ";
-			cin >> podstawa;
-			if (podstawa < 2 || podstawa > 9)
+			cin >> i_podstawa;
+			if (i_podstawa < 2 || i_podstawa > 9)
 				cout << "Podana podstawa nie nalezy do przedzialu 2-9." << endl;
-		} while (podstawa < 2 || podstawa > 9);
+		} while (i_podstawa < 2 || i_podstawa > 9);
 
-		wynik = 0;
+
+	}
+
+	void sprawdz_dlugosc() {
+		dlugosc = s_liczba.length();
+	}
+
+	void przelicz() {
+		int cyfra;
+		i_wynik = 0;
 		for (int i = 0; i < dlugosc; i++) {
 
-			cyfra = liczba[i] - int('0');
-			wynik = wynik * podstawa + cyfra;
+			cyfra = s_liczba[i] - int('0');
+			i_wynik = i_wynik * i_podstawa + cyfra;
 		}
-		cout << "Liczba: " << liczba << "(" << podstawa << ")" << " = "
-				<< wynik << "(10)\n";
+	}
+	void zwroc_wynik() {
+		cout << "Liczba: " << s_liczba << "(" << i_podstawa << ")" << " = "
+				<< i_wynik << "(10)\n";
+	}
+};
+
+int main() {
+
+
+	Na_dziesietny *system = new Na_dziesietny;
+	system->powitanie();
+
+	char repeat = 't'; //do pętli powtarzającej program
+	while (repeat == 't') { //pętla powtarzająca program dopóki użytkownik wpisze t
+
+		system->pobierz_dane();
+		system->sprawdz_dlugosc();
+		system->przelicz();
+		system->zwroc_wynik();
 		cout << "Czy chcesz cos jeszcze obliczyc? t/n" << endl;
 		cin >> repeat;
 	}
 	cout << "Koniec programu." << endl;
+	delete system;
 	return 0;
 }
 
